99699bf9448f92574731f843994eb55f4ed1c2c7
cbri92 <caterina.brighi@sydney.edu.au>
2023-01-25 16:20:07 +0100

    Updated Registration script

--- RegistrationCT_T2_DWI.py
+++ RegistrationCT_T2_DWI.py
@@ -19,7 +19,8 @@ from ImageStatisticsFunctions import *
 
 #%% Set Working directory
         
-data_supradir = 'C:/Users/cbri3325/OneDrive - The University of Sydney (Staff)/Caterina Brighi/Data/SacralChordoma_CNAO/' #Set working directory
+# data_supradir = 'C:/Users/cbri3325/OneDrive - The University of Sydney (Staff)/Caterina Brighi/Data/SacralChordoma_CNAO' #Set working directory
+data_supradir = 'C:/Users/cbri3325/OneDrive - The University of Sydney (Staff)/Caterina Brighi/Data/test/nifti/'
 
 subjs_path = [ f.path for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of the paths to the subjects directories
 subjs_name = [ f.name for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of subjects names
@@ -28,8 +29,51 @@ subjs_name = [ f.name for f in os.scandir(data_supradir) if f.is_dir() ] #Create
 
 for current in subjs_name:
 
-    subj_dir = data_supradir+current
-    if not os.path.exists(data_supradir+current+'/MRI/baseline/Prescribed_dose'):#if it does not already exist, create a directory where the optimized dose will be saved
-        os.mkdir(data_supradir+current+'/MRI/baseline/Prescribed_dose')
-    out_dir = data_supradir+current+'/MRI/baseline/Prescribed_dose'
+    subj_dir = data_supradir+current #Define path to patient directory
     
+    CT = sitk.ReadImage(subj_dir+'/ct.nii', sitk.sitkFloat32) #Read CT image
+    #Aggiungi trova GTV, CTV, PTV  
+    
+    MRI_dir = subj_dir+'/MRI/'
+    studies_path = [ f.path for f in os.scandir(MRI_dir) if f.is_dir() ] #Create a list of the paths to the MRI studies directories
+    studies_name = [ f.name for f in os.scandir(MRI_dir) if f.is_dir() ] #Create a list of studies names
+    
+    for study in studies_name:
+        
+        study_dir = MRI_dir+study
+        
+        #Create a directory to store the registration matrices
+        if not os.path.exists(study+'/regFiles'):#if it does not already exist, create a directory where the registration files will be saved
+            os.mkdir(study_dir+'/regFiles')
+        reg_dir = study_dir+'/regFiles'
+        
+        #Create a directory to store the registered images
+        if not os.path.exists(study+'/regImages'):#if it does not already exist, create a directory where the registered images will be saved
+            os.mkdir(study_dir+'/regImages')
+        regIm_dir = study_dir+'/regImages'
+        
+        #Create a directory to store the MRI info files
+        if not os.path.exists(study_dir+'/info/'):#if it does not already exist, create a directory where the info files will be saved
+            os.mkdir(study_dir+'/info/')
+        info_dir = study_dir+'/info/'
+        
+        #Move all the .bval, .bvec and .json files into the info folder
+        for filename in glob.glob(study_dir +'/' +'*.bval'):
+            shutil.move(filename, info_dir)
+            
+        for filename in glob.glob(study_dir +'/' +'*.bvec'):
+            shutil.move(filename, info_dir)
+            
+        for filename in glob.glob(study_dir +'/' +'*.json'):
+            shutil.move(filename, info_dir)
+            
+        #%%Registration of CT image to T2 MRI image
+        
+        #Read T2 transversal image       
+        T2_path = glob.glob(study_dir+'/'+'*t2_tse_tra*')[0]
+        T2 = sitk.ReadImage(T2_path, sitk.sitkFloat32)
+        
+        #Apply rigid body registration to register CT image to T2 image
+        CTinT2, CTinT2_tfm = RegisterResample_image(T2, CT, 'different', 'Multires')
+        sitk.WriteImage(CTinT2, regIm_dir+'/CTonT2.nii') #Save registered image in regImages directory
+        sitk.WriteTransform(CTinT2_tfm, reg_dir+'/xf_CTonT2.txt') #Save registration matrix in regFiles directory
