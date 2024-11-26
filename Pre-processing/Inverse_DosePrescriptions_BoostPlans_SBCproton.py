9c39f72311295464b32c2f077c5813a4f28baf4e
cbri92 <70302171+cbri92@users.noreply.github.com>
2024-11-26 17:43:38 +0100

    Update Inverse_DosePrescriptions_BoostPlans_SBCproton.py

--- Pre-processing/Inverse_DosePrescriptions_BoostPlans_SBCproton.py
+++ Pre-processing/Inverse_DosePrescriptions_BoostPlans_SBCproton.py
@@ -19,7 +19,7 @@ from ConvertNii_ToDoseFiles import * #Use this functions only when using the dic
 
 #%% Set Working directory
         
-data_supradir = 'C:/Users/cbri3325/OneDrive - The University of Sydney (Staff)/Caterina Brighi/Data/SBC_Tutti/' #Set working directory
+data_supradir = 'path/to/pathients/data/supra/directory/' #Set working directory
 
 subjs_path = [ f.path for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of the paths to the subjects directories
 subjs_name = [ f.name for f in os.scandir(data_supradir) if f.is_dir() ] #Create a list of subjects names
